e4fba717b1d72794cbb3ec9d33f8381a4f1f4787
AgostinaArevaloJaube <agosarevalo@gmail.com>
2020-12-28 14:55:56 -0300

    Actualiza nombre de componente

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Shop from '../pages/Shop';
 import Purchases from '../pages/Purchases';
-import GetPoints from '../pages/GetPoints';
+import GetCoins from '../pages/GetCoins';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -10,10 +10,9 @@ function App() {
 		<Router>
 			<Header />
 			<Switch>
-				<Route path="/" component={Shop} exact/>
-        {/* <Route path="/shop" component={Shop} /> */}
+				<Route path="/" component={Shop} exact />
 				<Route path="/purchases" component={Purchases} />
-				<Route path="/getpoints" component={GetPoints} />
+				<Route path="/getcoins" component={GetCoins} />
 			</Switch>
 		</Router>
 	);
