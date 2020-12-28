1002820a225a977f888758157268298dc6a8825a
AgostinaArevaloJaube <agosarevalo@gmail.com>
2020-12-28 14:55:04 -0300

    Agrega estilos globales y theme

---
+++ src/styles/globalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: 'Source Sans Pro', sans-serif;
+	color: '#616161'
+}
+`;
+
+export default GlobalStyle;
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+	colors: {
+		primary: '#0AD4FA',
+		background: '#F2F2F2',
+		boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+		buttonBackground: '#EDEDED'
+	},
+	fonts:{
+		primaryColor: '#616161',
+		secondaryColor: 'A3A3A3',
+	},
+	mediaQueries: {
+		above480: '(min-width: 480px)',
+		above600: '(min-width: 600px)',
+		above768: '(min-width: 768px)',
+		above900: '(min-width: 900px)',
+		above1024: '(min-width: 1024px)',
+		above1200: '(min-width: 1200px)',
+	}
+}
+
+export default theme
