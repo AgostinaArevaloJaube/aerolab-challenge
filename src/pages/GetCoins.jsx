c6703c29bb5e8c38f7eb6008b1b37be9fc60b1eb
AgostinaArevaloJaube <agosarevalo@gmail.com>
2020-12-28 17:34:10 -0300

    Crea página de adquisicion de monedas

--- src/pages/GetCoins.jsx
+++ src/pages/GetCoins.jsx
@@ -1,12 +1,59 @@
 import React from 'react';
-//import styled from 'styled-components';
+import styled from 'styled-components';
+import theme from '../styles/theme';
+import Coins from '../styles/Coins';
+import coin from '../assets/icons/coin.svg';
+import {PagesContainer, Title} from '../styles/PagesContainer'
 
 const GetCoins = () => {
-
-
 	return (
-		<h1>Hola soy GetCoins</h1>
-	)
-}
+		<PagesContainer>
+			<Title>Get more coins</Title>
+			<p>Choose the coins you want to add</p>
+
+			<CoinsContainer>
+				<Coin>
+					<p>1000</p>
+					<img src={coin} alt="Ilustration of coin" />
+				</Coin>
+				<Coin>
+					<p>5000</p>
+					<img src={coin} alt="Ilustration of coin" />
+				</Coin>
+				<Coin>
+					<p>7500</p>
+					<img src={coin} alt="Ilustration of coin" />
+				</Coin>
+			</CoinsContainer>
+		</PagesContainer>
+	);
+};
+
+const CoinsContainer = styled.div`
+	margin: 0 auto;
+	width: 80%;
+
+	@media ${theme.mediaQueries.above600}{
+		display: flex;
+		justify-content: center;
+	})
+`;
+
+const Coin = styled(Coins)`
+	margin: 1rem;
+	cursor: pointer;
+
+	&:hover {
+		background-color: ${theme.colors.background};
+	}
+
+	@media ${theme.mediaQueries.above600} {
+		width: 100%;
+	}
+
+	@media ${theme.mediaQueries.above1200} {
+		width: 50%;
+	}
+`;
 
 export default GetCoins;
