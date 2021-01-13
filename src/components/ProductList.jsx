3d96516374adad5cc793b52f68fb783b29af299c
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-01-12 21:55:14 -0300

    Setea renderizado de productos

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
-// import styled from 'styled-components';
-// import { getProducts } from '../services/api';
+import React, { useEffect, useState } from 'react';
+//import { ProductContext } from './Context/ProductContex';
+import Product from '../components/Product';
+import { getProducts } from '../services/api';
+
+import styled from 'styled-components';
 
 const ProductList = () => {
-	return <h1>Soy productlist</h1>;
+	const [productData, setProductData] = useState([]);
+
+	useEffect(() => {
+		getProducts(setProductData);
+	}, []);
+
+	return (
+		<ProductListContianer>
+			{productData.map((product) => (
+				<Product {...product} key={product._id} />
+			))}
+		</ProductListContianer>
+	);
 };
 
+export default ProductList;
 
-export default ProductList
+const ProductListContianer = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-content: center;
+`;
