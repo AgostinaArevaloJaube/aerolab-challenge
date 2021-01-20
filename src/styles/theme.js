0f12b9d2b4981579afbbf54b2e2fd979ba829296
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-01-20 17:43:11 -0300

    Agrega linear gradient

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,11 +3,13 @@ const theme = {
 		primary: '#0AD4FA',
 		background: '#F2F2F2',
 		boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.1)',
-		buttonBackground: '#EDEDED'
+		buttonBackground: '#EDEDED',
+		backgroundGradient:
+			'linear-gradient(180deg, rgba(10, 212, 250, 0.86) 0%, rgba(37, 187, 241, 0.86) 100%);'
 	},
-	fonts:{
+	fonts: {
 		primaryColor: '#616161',
-		secondaryColor: '#A3A3A3',
+		secondaryColor: '#A3A3A3'
 	},
 	mediaQueries: {
 		above480: '(min-width: 480px)',
@@ -15,8 +17,8 @@ const theme = {
 		above768: '(min-width: 768px)',
 		above900: '(min-width: 900px)',
 		above1024: '(min-width: 1024px)',
-		above1200: '(min-width: 1200px)',
+		above1200: '(min-width: 1200px)'
 	}
-}
+};
 
-export default theme
+export default theme;
