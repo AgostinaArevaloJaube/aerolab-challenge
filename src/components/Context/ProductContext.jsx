3256a4d28214d2b26a3a53c859db8679f54b8e3d
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-03-08 12:39:13 -0300

    Remove excess  code

--- src/components/Context/ProductContext.jsx
+++ src/components/Context/ProductContext.jsx
@@ -1,21 +1,10 @@
-import React, { useState, createContext, useEffect } from 'react';
-import { getProducts } from '../../services/api';
+import React, { useState, createContext, useEffect } from "react";
+import { getProducts } from "../../services/api";
 
 export const ProductContext = createContext();
 
 export const ProductProvider = (props) => {
-	const [productData, setProductData] = useState([
-		{
-			name: '',
-			_id: '',
-			cost: 0,
-			category: '',
-			img: {
-				hdUrl: '',
-				url: ''
-			}
-		}
-	]);
+	const [productData, setProductData] = useState([]);
 
 	useEffect(() => {
 		getProducts(setProductData);
