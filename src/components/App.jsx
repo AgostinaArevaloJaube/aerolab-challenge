4ca3a3b21b99aa690646ae3fff0a3de97d9039c3
AgostinaArevaloJaube <agosarevalo@gmail.com>
2021-03-08 12:35:26 -0300

    Order dependencies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import Header from '../components/Header';
-import Shop from '../pages/Shop';
-import Redeems from '../pages/Redeems';
-import GetCoins from '../pages/GetCoins';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import { UserProvider } from '../components/Context/UserContext';
-import { ProductProvider } from '../components/Context/ProductContext';
-import { FilterProvider } from './Context/FilterContext';
+import React from "react";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+import Shop from "../pages/Shop";
+import Redeems from "../pages/Redeems";
+import GetCoins from "../pages/GetCoins";
+import Header from "../components/Header";
+
+import { UserProvider } from "../components/Context/UserContext";
+import { FilterProvider } from "../components/Context/FilterContext";
+import { ProductProvider } from "../components/Context/ProductContext";
 
 function App() {
 	return (
